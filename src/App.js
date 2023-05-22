b1b37839409bb903a17cab1137561878db2c53e8
paschalc24 <pcorrigan@umass.edu>
2023-05-22 00:01:30 -0400

    Added primary app components and styling.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,88 @@
 import './App.css';
+import {useState} from 'react'
+import AudioRecorder from "./UIComponents/AudioRecorder"
+import background from './images/layer2.svg'
+import blob from './images/blob.svg'
+import TypeWriterEffect from 'react-typewriter-effect';
 
 function App() {
-  return (
-    <div className="App">
+  const [showBlob, setShowBlob] = useState(true);
+  const isMobile = window.innerWidth <= 767;
+  return showBlob ? (
+      <div className="App">
+        <div style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          width: '100vw',
+          height: '100vh',
+          zIndex: -1,
+          backgroundImage: `url(${background})`,
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+        }}/>
+      <header className="header-container">Vox Canvas 🎨</header>
+      <div className='blob'
+          style={{
+          backgroundImage: `url(${blob})`,
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+        }}>
+        <TypeWriterEffect
+        textStyle={{ 
+          color: "black", 
+          fontFamily: 'Mulish', 
+          fontSize: isMobile ? '15px' : '25px' 
+        }}
+        className="typewriter"
+        startDelay={0}
+        cursorColor= {"transparent"}
+        loop="false"
+        text={"Hello, welcome to Vox Canvas! \n Press and hold the record button to create AI generated art!"}
+        typeSpeed={60}
+        eraseSpeed={0}
+        />
+        <button onClick={() => setShowBlob(false)} className='enter-button' style={{
+          backgroundImage: `url(${background})`,
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+        }}>👩‍🎨 👨‍🎨</button>
+      </div>
+      <div className="footer-container">
+        <footer style={{fontSize:"15px", fontWeight:"bold"}}>
+          <br></br>
+          <a style={{textDecoration:"none", color:"#FFFFFF"}} rel="noreferrer noopener" target="_blank" href="https://www.linkedin.com/in/paschalcorrigan/">LinkedIn 👥</a>
+          <span style={{color:"#F5F5F5"}}> ☞ </span>
+          <a style={{textDecoration:"none", color:"#FFFFFF"}} rel="noreferrer noopener" target="_blank" href="https://github.com/paschalc24/evil-gpt">GitHub 💻</a><br></br>
+        </footer>
+      </div>
     </div>
-  );
+  ) : (
+    <div className="App">
+      <div style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100vw',
+        height: '100vh',
+        zIndex: -1,
+        backgroundImage: `url(${background})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+      }}>
+    </div>
+    <header className="header-container">Vox Canvas 🎨</header>
+    <AudioRecorder/>
+    <div className="footer-container">
+      <footer style={{fontSize:"15px", fontWeight:"bold"}}>
+        <br></br>
+        <a style={{textDecoration:"none", color:"#FFFFFF"}} rel="noreferrer noopener" target="_blank" href="https://www.linkedin.com/in/paschalcorrigan/">LinkedIn 👥</a>
+        <span style={{color:"#F5F5F5"}}> ☞ </span>
+        <a style={{textDecoration:"none", color:"#FFFFFF"}} rel="noreferrer noopener" target="_blank" href="https://github.com/paschalc24/evil-gpt">GitHub 💻</a><br></br>
+      </footer>
+    </div>
+  </div>
+);
 }
 
 export default App;
---
+++ src/UIComponents/AudioRecorder.js
@@ -0,0 +1,162 @@
+import React, { useState, useRef, useEffect, useCallback } from 'react';
+import '../CSSComponents/AudioRecorder.css';
+import axios from 'axios';
+import spinner from '../images/LCPT.gif'
+import TypeWriterEffect from 'react-typewriter-effect';
+import loadingAnimation from '../images/Wedges-3s-200px.svg'
+
+
+const AudioRecorder = () => {
+ const mediaRecorderRef = useRef(null);
+ const chunksRef = useRef([]);
+ const [isRecording, setIsRecording] = useState(false);
+ const [isLoading, setIsLoading] = useState(false);
+ const [imgURL, setImgURL] = useState('');
+ const [userRequest, setUserRequest] = useState('');
+ const isMobile = window.innerWidth <= 767;
+
+
+ const handleMouseDown = () => {
+   navigator.mediaDevices
+     .getUserMedia({ audio: true })
+     .then((stream) => {
+       const mediaRecorder = new MediaRecorder(stream);
+       mediaRecorder.ondataavailable = (event) => {
+         if (event.data && event.data.size > 0) {
+           chunksRef.current.push(event.data);
+         }
+       };
+       mediaRecorder.start();
+       mediaRecorderRef.current = mediaRecorder;
+       setIsRecording(true);
+     })
+     .catch((error) => {
+       console.error('Error accessing media devices:', error);
+     });
+ };
+
+
+ const handleImageRequest = (transcription) => {
+   try {
+       const requestData = {
+           prompt: transcription,
+           n: 1,
+           size: "512x512"
+         }; 
+       axios.post(
+           'https://api.openai.com/v1/images/generations',
+           requestData,
+           {
+           headers: {
+               Authorization: 'Bearer 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',
+               'Content-Type': 'application/json',
+           },
+           }
+       ).then (response => {
+           console.log(response.data.data[0].url)
+           setImgURL(response.data.data[0].url)
+           setIsLoading(false);
+       });
+   } catch (error) {
+       console.log('Error Generating Image:', error);
+       setIsLoading(false);
+   }
+ }
+
+
+ const handleMouseUp = useCallback(() => {
+   if (isRecording) {
+     if (mediaRecorderRef.current) {
+       mediaRecorderRef.current.onstop = () => {
+         const blob = new Blob(chunksRef.current, { type: 'audio/mp3' });
+         const file = new File([blob], 'recording.mp4', { type: 'audio/mp3' });
+       try {
+           const formData = new FormData();
+           formData.append('file', file);
+           formData.append('model', 'whisper-1');      
+           axios.post(
+               'https://api.openai.com/v1/audio/translations',
+               formData,
+               {
+               headers: {
+                   Authorization: 'Bearer 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',
+                   'Content-Type': 'multipart/form-data',
+               },
+               }
+           ).then (response => {
+               setUserRequest(response.data.text)
+               setIsLoading(true)
+               handleImageRequest(response.data.text);
+           });
+       } catch (error) {
+           console.log('Error Transcribing Audio:', error);
+       }
+       };
+       mediaRecorderRef.current.stop();
+     }
+     chunksRef.current = [];
+     setTimeout(setIsRecording(false), 3000);
+   }
+ }, [isRecording]);
+
+
+ useEffect(() => {
+   // Add event listener to handle mouseup on document level
+   document.addEventListener('mouseup', handleMouseUp);
+   // Cleanup the event listener on component unmount
+   return () => {
+     document.removeEventListener('mouseup', handleMouseUp);
+   };
+ }, [handleMouseUp]);
+
+
+ return (
+   <div>
+     {isLoading ? (
+       <div className="image-container">
+           <div style={{
+             width: '60px',
+             height: '60px',
+             zIndex: -1,
+             backgroundImage: `url(${loadingAnimation})`,
+             backgroundSize: 'cover',
+             backgroundRepeat: 'repeat',
+           }}/>
+       </div>
+       ) : (
+       <div className="image-container">
+         <img className="image-border" alt="" src={imgURL}/>
+         <TypeWriterEffect
+           textStyle={{ 
+             color: "#FFFFFF", 
+             fontFamily: 'Mulish', 
+             position:'absolute', 
+             fontSize: isMobile ? '15px' : '25px', 
+             top: isMobile ? '62.5vh': '60vh',
+             left: isMobile ? '35vw' : ''
+            }}
+           className="typewriter"
+           startDelay={0}
+           cursorColor= {"transparent"}
+           loop="false"
+           text={userRequest}
+           typeSpeed={60}
+           eraseSpeed={0}
+           />
+       </div>
+     )}
+     <button
+       className="button-container"
+       onMouseDown={handleMouseDown}
+       disabled={isRecording}
+     >
+     <div className="record-button">
+       <img className="spinner-gif" alt='' src={spinner}/>
+     </div>
+     </button>
+   </div>
+ );
+};
+
+
+export default AudioRecorder;
